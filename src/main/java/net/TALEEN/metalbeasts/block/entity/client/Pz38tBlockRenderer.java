db8196a3566b1de709e0b6b2a61e575de3806bc4
TALEENcmb <166990708+TALEENcmb@users.noreply.github.com>
2024-04-14 17:25:54 +0200

    Updated Pz.38(t) model
    The model of the Pz.38(t) has been updated to prevent Z fighting and the scale has been fixed in Pz38tBlockRenderer to fit the description of the vehicle.

--- src/main/java/net/TALEEN/metalbeasts/block/entity/client/Pz38tBlockRenderer.java
+++ src/main/java/net/TALEEN/metalbeasts/block/entity/client/Pz38tBlockRenderer.java
@@ -1,12 +1,19 @@
 package net.TALEEN.metalbeasts.block.entity.client;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.TALEEN.metalbeasts.block.entity.Pz38tBlockEntity;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoBlockRenderer;
 
 public class Pz38tBlockRenderer extends GeoBlockRenderer<Pz38tBlockEntity> {
     public Pz38tBlockRenderer(BlockEntityRendererProvider.Context context) {
         super(new Pz38tBlockModel());
+    }
+
+    @Override
+    public void scaleModelForRender(float widthScale, float heightScale, PoseStack poseStack, Pz38tBlockEntity animatable, BakedGeoModel model, boolean isReRender, float partialTick, int packedLight, int packedOverlay) {
+        super.scaleModelForRender(1.55f, 1.55f, poseStack, animatable, model, isReRender, partialTick, packedLight, packedOverlay);
     }
 }
 
